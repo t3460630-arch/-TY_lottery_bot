4d7341079d9df06fc138375ef2aa6acd5fe30c88
t3460630-arch <t3460630@gmail.com>
2026-06-17 15:21:19 +0330

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,35 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import random
+
+participants = set()
+
+BOT_TOKEN = "PUT_YOUR_TOKEN_HERE"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("سلام 💜 ربات قرعه‌کشی روشنه")
+
+async def join(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user = update.effective_user.id
+
+    if user in participants:
+        await update.message.reply_text("قبلاً وارد شدی ❌")
+    else:
+        participants.add(user)
+        await update.message.reply_text("ثبت شدی ✔️")
+
+async def draw(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if not participants:
+        await update.message.reply_text("هیچ کسی نیست ❌")
+        return
+
+    winner = random.choice(list(participants))
+    await update.message.reply_text(f"برنده 🎉: {winner}")
+
+app = ApplicationBuilder().token(BOT_TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("join", join))
+app.add_handler(CommandHandler("draw", draw))
+
+app.run_polling()
